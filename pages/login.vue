171325f0796d73cbb703f7899f32e89ad6d68af4
kogachan4 <kogachan4@gmail.com>
2019-11-30 10:16:25 +0900

    loginのvueファイル

---
+++ pages/login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="login-form">
+    <form @submit.prevent="login">
+      <p class="error" v-if="error">{{ error }}</p>
+      <p><input type="text" v-model="email" placeholder="email" name="email"/></p>
+      <p><input type="text" v-model="password" placeholder="password" name="password"/></p>
+      <div class="login-btn">
+        <button type="submit">ログイン</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        error: null,
+        email: "",
+        password: "",
+      }
+    },
+    methods: {
+      async login() {
+        try {
+          await this.$store.dispatch("login", {
+            email: this.email,
+            password: this.password
+          })
+          this.$router.push("/")
+        } catch(e) {
+          this.error = e.message
+        }
+      }
+    }
+  }
+</script>
