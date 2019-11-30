930a1ef25ec30bba8b0780583772cb28a543ba43
kogachan4 <kogachan4@gmail.com>
2019-11-30 11:02:58 +0900

    #2  ログイン機能

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="login-form">
+    <form @submit.prevent="login">
+      <p class="error" v-if="error">{{ error }}</p>
+      <p><input type="text" v-model="email" placeholder="email" name="email"/></p>
+      <p><input type="text" v-model="password" placeholder="password" name="password"/></p>
+      <div class="login-btn">
+        <button type="submit">ログイン</button>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    data() {
+      return {
+        error: null,
+        email: "",
+        password: "",
+      }
+    },
+    methods: {
+      async login() {
+        try {
+          await this.$store.dispatch("login", {
+            email: this.email,
+            password: this.password
+          })
+          this.$router.push("/")
+        } catch(e) {
+          this.error = e.message
+        }
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  background-color:slategray;
+}
+.login-form{
+  width:500px;
+  height :auto;
+  margin:0 auto;
+  margin-top: 150px;
+}
+input{
+  font-size:30px;
+}
+button{
+  font-size:20px;
+  margin-left: 100px;
 }
 </style>
