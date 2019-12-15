b3f33b9145a2fdc8832e1cbca2073d3913f3845c
kogachan4 <kogachan4@gmail.com>
2019-12-15 17:22:12 +0900

    js

--- store/index.js
+++ store/index.js
@@ -1,14 +1,3 @@
- import Vuex from "vuex"
- import mutations from "./mutations"
- import action from "./actions"
-
 export const state = () => {
-  return new Vuex.Store({
-    state: {
-      authUser: null
-    },
-    mutations,
-    actions
-  })
+    
 }
-export default store
