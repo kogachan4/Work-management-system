bdcb01b10b7c5fbed45112c0e7b327c907de1154
kogachan4 <kogachan4@gmail.com>
2019-12-17 11:34:03 +0900

    変更

--- pages/index.vue
+++ pages/index.vue
@@ -1,47 +1,17 @@
 <template>
   <div class="container">
       <h1 class="mt-3 mb-5">勤務管理</h1>
-      <form @submit.prevent="login" class="form-signin">
-        <p class="error" v-if="error">{{ error }}</p>
+      <form  class="form-signin">
         <label for="Email" class="sr-only">Email address</label>
-        <input type="text" v-model="email" id="Email" class="form-control input" placeholder="email" name="email">
+        <input type="text"  id="Email" class="form-control input" placeholder="email" name="email">
         <label for="Password" class="sr-only">Password</label>
-        <input type="text" v-model="password" id="Password" class="form-control input mb-4" placeholder="password" name="password">
-        <div class="checkbox mb-3">
-        </div>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">ログイン</button>
-        <p class="mt-5 mb-3 text-muted"></p>
+        <input type="text" id="Password" class="form-control input mb-4" placeholder="password" name="password">
+        <a class="btn btn-light btn-block" tabindex="" >ログイン</a>
       </form>
   </div>
 </template>
 
 <script>
-import Header from '~/components/Header.vue'
-  export default {
-    data() {
-      return {
-        error: null,
-        email: "",
-        password: "",
-      }
-    },
-    methods: {
-      async login() {
-        try {
-          await this.$store.dispatch("login",{
-            email: this.email,
-            password: this.password
-          })
-          this.$router.push("/")
-        } catch (e) {
-          this.error = e.message
-        }
-      }
-    },
-    components:{
-        Header
-    }
-  }
 </script>
 
 <style lang="scss" scoped>
--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,41 +1,25 @@
 <template>
-<div class="container">
-<body class="text-center">
-<h1 class="mt-3">勤務管理</h1>
-<form class="form-signin">
-  <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
-  <label for="inputEmail" class="sr-only">Email address</label>
-  <input type="email" id="inputEmail" class="form-control input" placeholder="Email address" required autofocus>
-  <label for="inputPassword" class="sr-only">Password</label>
-  <input type="password" id="inputPassword" class="form-control input mb-4" placeholder="Password" required>
-  <div class="checkbox mb-3">
-    <label>
-      <input type="checkbox" value="remember-me"> Remember me
-    </label>
+  <div class="container">
+      <h1 class="mt-3 mb-5">勤務管理</h1>
+      <form  class="form-signin">
+        <label for="Email" class="sr-only">Email address</label>
+        <input type="text"  id="Email" class="form-control input" placeholder="email" name="email">
+        <label for="Password" class="sr-only">Password</label>
+        <input type="text" v-model="password" id="Password" class="form-control input mb-4" placeholder="password" name="password">
+        <a class="btn btn-light btn-block" tabindex="" @click="login">ログイン</a>
+      </form>
   </div>
-  <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-  <p class="mt-5 mb-3 text-muted"></p>
-</form>
-</body>
-</div>
 </template>
 
 <script>
-import Header from '~/components/Header.vue'
-  export default {
-    components:{
-        Header
-    }
-  }
 </script>
 
 <style lang="scss" scoped>
 .btn{
   width:300px;
-  margin-left: 135px;
+  margin-left: 40px;
 }
 .input{
   width:400px;
-  margin-left: 100px;
 }
 </style>
